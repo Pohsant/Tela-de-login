8ebdb5530034aa336f2add19178df5a5bdeafbb4
Matheus Santiago <107511424+Pohsant@users.noreply.github.com>
2022-06-19 23:27:47 -0300

    Add files via upload

---
+++ home-screen/assets/index.js
@@ -0,0 +1,34 @@
+const darkModeClass = 'dark-mode';
+const lua = document.querySelector('#lua');
+const sol= document.querySelector('#sol');
+const first = document.querySelector('.first-side');
+const second = document.querySelector('.second-side');
+const button = document.querySelector('button');
+const fh1 = document.querySelector('#first-h1');
+const sh1 = document.querySelector('#second-h1');
+const btn2 = document.querySelector('#second-btn');
+const p1 = document.querySelector('#p1');
+const p2 = document.querySelector('#p2');
+const p3 = document.querySelector('#p3');
+
+function changeMode(){
+    changeClasses();
+}
+
+function changeClasses(){
+    lua.classList.toggle(darkModeClass);
+    sol.classList.toggle(darkModeClass);
+    first.classList.toggle(darkModeClass);
+    second.classList.toggle(darkModeClass);
+    button.classList.toggle(darkModeClass);
+    fh1.classList.toggle(darkModeClass);
+    sh1.classList.toggle(darkModeClass);
+    p1.classList.toggle(darkModeClass);
+    p2.classList.toggle(darkModeClass);
+    btn2.classList.toggle(darkModeClass)
+    p3.classList.toggle(darkModeClass);
+   
+}
+
+lua.addEventListener('click', changeMode);
+sol.addEventListener('click', changeClasses);
